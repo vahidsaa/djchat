5a68c652e098aacd6b8be061f01e44c081c8f14d
vahidsaa <vahidsalehi.b@gmail.com>
2023-10-24 21:40:49 +0330

    add pillow and validate for image

--- backend/server/models.py
+++ backend/server/models.py
@@ -2,9 +2,19 @@ from django.db import models
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
+from server.validate import validate_icon_image_size, validate_image_file_extension
 
 
-#for icon
+
+#for icon in server
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icon/{filename}"
+
+def server_banner_upload_path(instance, filename):
+    return f"server/{instance.id}/category_banner/{filename}"
+
+
+#for icon in category
 def category_icon_upload_path(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
 
@@ -13,7 +23,7 @@ def category_icon_upload_path(instance, filename):
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    # add icon and banners but firs must be add media
+    # add icon and banners but firs must be add media *we add filefield becuse pillow not support svg file for icon
     icon = models.FileField(upload_to=category_icon_upload_path ,null=True, blank='True', )
 
     #for (if add another icon delete first icon and add new)
@@ -57,12 +67,35 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name='channel_server')
+    banner = models.ImageField(upload_to=server_banner_upload_path, null=True, blank=True, validators=[validate_image_file_extension])
+    icon =  models.ImageField(upload_to=server_icon_upload_path, null=True, blank=True, validators=[validate_icon_image_size, validate_image_file_extension])
 
     #for how saved in database (lower case)
-    def save(self, *args, **kwargs):
-        self.name = self.name.lower()
-        super(Channel, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.name = self.name.lower()
+    #     super(Channel, self).save(*args, **kwargs)
 
+    #copy def save and signal for delete (category model) 
     
+    def save(self, *args, **kwargs):
+        if self.id: # thats mean we have a image befor
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    #for if delete category also delete icon with signals
+    @receiver(models.signals.pre_delete, sender='server.Server')
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == "banner":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
+
+
+
     def __str__(self):
         return self.name
---
+++ backend/server/validate.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from django.core.exceptions import ValidationError
+import os
+
+
+def validate_icon_image_size(image):
+    if image:
+        with Image.open(image) as img :
+            if img.width > 70 or omg.height > 70 :
+                raise ValidationError(
+                    f"the maximum allowed dimensions for the img are 70x70 . your of image you uploded {img.size} "
+                )
+
+
+def validate_image_file_extension(value):
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = [".jpg", ".jpeg", ".png", ".gif"]
+    if not ext.lower() in valid_extensions:
+        raise ValidationError("Unsupported file extension. must be (jpeg , jpg , png , gif)")
